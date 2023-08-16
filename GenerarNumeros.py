05260d5225d299bcda82fb9bcd1159a14c6c2868
SantyPrado01 <santiago01prado@gmail.com>
2023-08-16 17:49:20 -0300

    Proyecto GenerarNumeros Finalizado

--- GenerarNumeros.py
+++ GenerarNumeros.py
@@ -10,6 +10,7 @@ def generar_numero():
 
 ventana = Tk()
 ventana.title('Generar Numeros')
+ventana.resizable(width=False, height=False)
 
 numeros_inicio = 1
 numeros_fin = 20
@@ -22,25 +23,25 @@ numero_seleccionado_uno.set(numeros_lista[0])
 numero_seleccionado_dos = IntVar()
 numero_seleccionado_dos.set(numeros_lista[0])
 
-texto_numeros = Label(ventana, text='Elige dos numeros y se generara un numero al azar entre ellos.')
-texto_numeros.grid(column=0, row=0, columnspan=2)
+texto_numeros = Label(ventana, text='Elige dos numeros y se generara un numero al azar entre ellos.', font=12)
+texto_numeros.grid(column=0, row=0, columnspan=2,pady=8, ipady=8, padx=10, ipadx=10)
 
-texto_numero_uno = Label(ventana, text='Elige el numero 1')
-texto_numero_uno.grid(column=0, row=1)
+texto_numero_uno = Label(ventana, text='Elige el numero 1', font=12)
+texto_numero_uno.grid(column=0, row=1,pady=8, ipady=8, padx=10, ipadx=10)
 
 numeros_desplegable_uno = OptionMenu(ventana, numero_seleccionado_uno, *numeros_lista)
-numeros_desplegable_uno.grid(column=1, row=1)
+numeros_desplegable_uno.grid(column=1, row=1,pady=8, ipady=8, padx=10, ipadx=10)
 
-texto_numero_dos = Label(ventana, text='Elige el numero 2')
-texto_numero_dos.grid(column=0, row=2)
+texto_numero_dos = Label(ventana, text='Elige el numero 2', font=12)
+texto_numero_dos.grid(column=0, row=2,pady=8, ipady=8, padx=10, ipadx=10)
 
 numeros_desplegable_dos = OptionMenu(ventana, numero_seleccionado_dos, *numeros_lista)
-numeros_desplegable_dos.grid(column=1, row=2)
+numeros_desplegable_dos.grid(column=1, row=2,pady=8, ipady=8, padx=10, ipadx=10)
 
-texto_numero_generado = Label(ventana, text='Numero generado: ')
-texto_numero_generado.grid(column=0, row=3)
+texto_numero_generado = Label(ventana, text='Numero generado: ', font=12)
+texto_numero_generado.grid(column=0, row=3,pady=8, ipady=8, padx=10, ipadx=10)
 
-boton_generar = Button(ventana, text='Generar Numero', command=generar_numero)
-boton_generar.grid(column=0, row=4, columnspan=2)
+boton_generar = Button(ventana, text='Generar Numero', command=generar_numero, font=12)
+boton_generar.grid(column=0, row=4, columnspan=2,pady=8, ipady=8, padx=10, ipadx=10)
 
 ventana.mainloop()
